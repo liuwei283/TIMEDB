1675f607c3f095a03ffed97290b9b2d05d1c0828
FENG Xikang <xikanfeng2@gmail.com>
2020-08-09 19:35:19 +0800

    link deepomics user folder to public/data

--- app/controllers/submit_controller.rb
+++ app/controllers/submit_controller.rb
@@ -2,6 +2,7 @@ class SubmitController < ApplicationController
   UID = 39
   PROJECT_ID = 259
   TASK_ID = 1371
+  info = {"status"=>"success", "message"=>{"status"=>"finished", "inputs"=>[{"id"=>945, "name"=>"MetaPhlan results", "desc"=>"MetaPhlan results", "files"=>[{"name"=>"PH_sample_bacteria.xls", "path"=>"/data"}]}], "outputs"=>[{"id"=>766, "name"=>"PCoA, Shannon diversity, Top10 compositions and Comparison", "desc"=>"PCoA, Shannon diversity, Top10 compositions and Comparison", "files"=>[{"name"=>"pcoa.pdf", "path"=>"/project/TestProject/task_20200809184940"}, {"name"=>"Shannon.pdf", "path"=>"/project/TestProject/task_20200809184940"}, {"name"=>"histogram.pdf", "path"=>"/project/TestProject/task_20200809184940"}, {"name"=>"Top10_boxplot.pdf", "path"=>"/project/TestProject/task_20200809184940"}]}], "params"=>[{"id"=>1711, "name"=>"Target region", "prefix"=>"-r", "default"=>"CHN", "desc"=>"Select the samples from the target region in this oral microbiome database, e.g. CHN, JPN, PH or USA.", "value"=>"CHN"}]}}
   def index
     id = params[:id]
     gon.push id: id
@@ -15,48 +16,48 @@ class SubmitController < ApplicationController
       code: false,
       data: ''
     }
-    # begin
-    app_id = params[:app_id]
-    app_inputs = params[:inputs]
-    app_params = params[:params]
-    inputs = Array.new
-    params = Array.new
+    begin
+      app_id = params[:app_id]
+      app_inputs = params[:inputs]
+      app_params = params[:params]
+      inputs = Array.new
+      params = Array.new
 
-    # store input file to user's data folder
-    app_inputs&.each do |k, v|
-      uploader = JobInputUploader.new
-      uploader.store!(v)
-      inputs.push({
-        k => '/data/' + v.original_filename,
-      })
-    end
-    app_params&.each do |p|
-      p.each do |k, v|
-        params.push({
-          k => v,
+      # store input file to user's data folder
+      app_inputs&.each do |k, v|
+        uploader = JobInputUploader.new
+        uploader.store!(v)
+        inputs.push({
+          k => '/data/' + v.original_filename,
         })
       end
-    end
-    # submit task
-    client = LocalApi::Client.new
-    result = client.run_module(UID, PROJECT_ID, app_id.to_i, inputs, params)
-    Rails.logger.info(result)
-    if result['message']['code']
-      result_json[:code] = true
-      result_json[:data] = {
-        'msg': result['message']['data']['msg'],
-        'task_id': encode(result['message']['data']['task_id'])
-      }
-    else
+      app_params&.each do |p|
+        p.each do |k, v|
+          params.push({
+            k => v,
+          })
+        end
+      end
+      # submit task
+      client = LocalApi::Client.new
+      result = client.run_module(UID, PROJECT_ID, app_id.to_i, inputs, params)
+      Rails.logger.info(result)
+      if result['message']['code']
+        result_json[:code] = true
+        result_json[:data] = {
+          'msg': result['message']['data']['msg'],
+          'task_id': encode(result['message']['data']['task_id'])
+        }
+      else
+        result_json[:code] = false
+        result_json[:data] = {
+          'msg': result['message']
+        }
+      end
+    rescue StandardError => e
       result_json[:code] = false
-      result_json[:data] = {
-        'msg': result['message']
-      }
+      result_json[:data] = e.message
     end
-    # rescue StandardError => e
-    #   result_json[:code] = false
-    #   result_json[:data] = e.message
-    # end
     render json: result_json
   end
 
