1cb903f4417a4773ca976abbfa2c509c9a5d0e08
CHE_Lijia <lijiache2@gmail.com>
2020-12-08 14:38:48 +0800

    fix some bugs

--- app/controllers/samples_controller.rb
+++ app/controllers/samples_controller.rb
@@ -86,7 +86,7 @@ class SamplesController < ApplicationController
     def export_selected_metadata_dataset
         id = cookies.encrypted[:user]
         @user = User.find(id)
-        user_dir = File.join($stor_dir, id.to_s)
+        user_dir = File.join($user_stor_dir, id.to_s)
         ds_name = params[:ds_selected]  
         ds_dir = File.join(user_dir, ds_name) 
         @samples = Sample.order(:sample_name)
@@ -102,7 +102,7 @@ class SamplesController < ApplicationController
     def export_selected_abd_dataset
         id = cookies.encrypted[:user]
         @user = User.find(id)
-        user_dir = File.join($stor_dir, id.to_s)
+        user_dir = File.join($user_stor_dir, id.to_s)
         ds_name = params[:ds_selected]  
         ds_dir = File.join(user_dir, ds_name) 
         @project = Project.find(params[:project_id])
