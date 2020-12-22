30710406361f801cdaa5ffd5b554c8d625215a48
王砚斐 <minties@mintys-MacBook-Pro.local>
2020-12-22 16:59:00 +0800

    update relation mapping

--- app/models/analysis_user_datum.rb
+++ app/models/analysis_user_datum.rb
@@ -2,4 +2,23 @@ class AnalysisUserDatum < ApplicationRecord
     belongs_to :analysis
     belongs_to :user
     belongs_to :task_output, optional: true
+
+    def self.createDefaultDatum (analysis_id, user_id, task_output_id=nil)
+        @analysis = Analysis.find(analysis_id)
+        @analysisUserDatum = @analysis.analysis_user_data.new
+        default_chosen = {}.tap { |x|
+            @analysis.files_info.each do |dataType, info|
+                next unless !VizDataSource.find_by(data_type:dataType).optional
+                x[dataType] = nil
+            end
+        } 
+        @analysisUserDatum.chosen =  default_chosen
+        @analysisUserDatum.user = User.find(user_id)
+        if !task_output_id.blank?
+            @task_output = TaskOutput.find(task_output_id)
+            @analysis_user_datum.task_output = @task_output
+        end
+        @analysisUserDatum.save!
+        return @analysis_user_datum
+    end
 end
