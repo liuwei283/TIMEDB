35aae0a7de8b709f2411282fd6552ffa1e45c396
王砚斐 <minties@mintys-MacBook-Pro.local>
2020-12-23 15:31:47 +0800

    fix analysis missing viz mode

--- app/controllers/analysis_controller.rb
+++ app/controllers/analysis_controller.rb
@@ -12,19 +12,20 @@ class AnalysisController < ApplicationController
         chosen_output = nil
         chosen_output = @analysisUserDatum.task_output.id if !@analysisUserDatum.task_output.blank?
         gon.push module_name: @analysis.visualizer.js_module_name,
-                 analysis_name: @analysis.name,
-                 required_data: files_info.keys, 
-                 chosen_output: chosen_output,
-                 urls: {
-                    use_demo: api_analysis_use_demo_path(@analysis),
-                    use_task_output: api_analysis_use_task_output_path(@analysis),
-                    create_file: api_analysis_create_files_path(@analysis),
-                    all_files: api_analysis_all_files_path(@analysis),
-                    all_task_outputs: api_analysis_all_task_outputs_path(@analysis),
-                    chosen_files: api_analysis_chosen_files_path(@analysis),
-                    chosen_file_paths: api_analysis_chosen_file_paths_path(@analysis),
-                    batch_delete_files: api_analysis_batch_delete_files_path(@analysis),
-                 }
+                viz_mode: "analysis",
+                analysis_name: @analysis.name,
+                required_data: files_info.keys, 
+                chosen_output: chosen_output,
+                urls: {
+                use_demo: api_analysis_use_demo_path(@analysis),
+                use_task_output: api_analysis_use_task_output_path(@analysis),
+                create_file: api_analysis_create_files_path(@analysis),
+                all_files: api_analysis_all_files_path(@analysis),
+                all_task_outputs: api_analysis_all_task_outputs_path(@analysis),
+                chosen_files: api_analysis_chosen_files_path(@analysis),
+                chosen_file_paths: api_analysis_chosen_file_paths_path(@analysis),
+                batch_delete_files: api_analysis_batch_delete_files_path(@analysis),
+                }
 
     end
 
