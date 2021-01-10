d01b470c0e676a934db0e5b27c6af0dee4fa2693
王砚斐 <minties@172-15-0-60.lightspeed.stlsmo.sbcglobal.net>
2021-01-10 14:59:12 +0800

    add clear inactive user data scheduler

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
     has_many :analysis_user_data, dependent: :destroy
     has_many :viz_file_objects, dependent: :destroy
     has_many :task_outputs, dependent: :destroy
+    
+    def self.clear_inactive_users
+        Rails.logger.info 'start cleaning inactive user data'
+        @users = User.where "updated_at < ?", 2.weeks.ago
+        cleaned_user = []
+        @users.each do |user|
+            FileUtils.rm_rf "#{Rails.root}/data/user/#{user.id}" if Dir.exist? "#{Rails.root}/data/user/#{user.id}"
+            cleaned_user << user.id
+            user.destroy
+        end
+        Rails.logger.info "inactive user #{cleaned_user} cleared successfully"
+    end
 end
