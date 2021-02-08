15e8d50b54e53e3010f59816b98a6b16d62b52d5
WANG Yanfei <mintywangtvt@gmail.com>
2021-02-08 23:01:00 +0800

    updates

--- app/javascript/utils/bio-info.ts
+++ app/javascript/utils/bio-info.ts
@@ -4,6 +4,10 @@ export const rankDict = {
     c: "Class",
     o: "Order",
     f: "Family",
+    t: "tribe",
     g: "Genus",
     s: "Species",
 };
+ export function sortByRankKey(a: string, b: string): number {
+    return Object.keys(rankDict).indexOf(a) - Object.keys(rankDict).indexOf(b);
+ }
