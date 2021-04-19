63c20eb2104df6ee486e89ba77c1547e780a31d8
WANG Yanfei <mintywangtvt@gmail.com>
2021-04-19 15:23:37 +0800

    remove link to node_modules when deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -36,7 +36,7 @@ append :linked_files, "config/master.key"
 # append :linked_files, "config/database.yml"
 
 # Default value for linked_dirs is []
-append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/data", "public/packs", "node_modules", "data"
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/data", "public/packs", "data"
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
