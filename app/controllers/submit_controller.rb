50902aa5ed90ce7ab5ef6ec5247234ef3c5dadb6
WANG Yanfei <mintywangtvt@gmail.com>
2021-05-17 13:58:43 +0800

    pipeline query debug

--- app/controllers/submit_controller.rb
+++ app/controllers/submit_controller.rb
@@ -15,6 +15,8 @@ class SubmitController < ApplicationController
       client = LocalApi::Client.new
       result = client.task_info(UID, 235, 'app')
       Rails.logger.info result
+      result = client.task_info(UID, 235, 'pipeline')
+      Rails.logger.info result
       @result_message = result
       # result_json[:data] = result
     rescue StandardError => e
