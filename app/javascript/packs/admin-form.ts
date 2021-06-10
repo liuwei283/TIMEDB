1dff288015156bf4aa0426ffcc4312ea112d147f
WANG Yanfei <mintywangtvt@gmail.com>
2021-06-10 15:40:54 +0800

    add json input formatter for admin analyses

--- app/javascript/packs/admin-form.ts
+++ app/javascript/packs/admin-form.ts
@@ -1,4 +1,11 @@
+/// <reference types="ace" />
 import * as $ from "jquery";
+
+declare global {
+    interface Window {
+        ace: AceAjax.Ace;
+    }
+}
 function addRmBtnAction() {
     $(".btn-rm-opt").each(function() {
         const rmBtn = $(this);
@@ -21,14 +28,61 @@ function fixIndex() {
     });
 }
 
-$(".btn-add-opt").each(function() {
-    const addBtn = $(this);
-    const container = addBtn.siblings(".form-opt-container")[0];
-    const template = addBtn.siblings("template")[0] as HTMLTemplateElement;
-    addBtn.on("click", (e) => {
-        container.appendChild(document.importNode(template.content, true));
-        addRmBtnAction();
-        fixIndex();
+document.addEventListener("DOMContentLoaded", () => {
+    $(".btn-add-opt").each(function() {
+        const addBtn = $(this);
+        const container = addBtn.siblings(".form-opt-container")[0];
+        const template = addBtn.siblings("template")[0] as HTMLTemplateElement;
+        addBtn.on("click", (e) => {
+            container.appendChild(document.importNode(template.content, true));
+            addRmBtnAction();
+            fixIndex();
+        });
     });
 
+    $("*[data-ace]").each(function() {
+        console.log("test message");
+        const input = $(this);
+        const form = input.parents("form");
+        const mode = input.data("ace-mode");
+        const id = input.attr("id");
+        const name = input.attr("name");
+        const editor = window.ace.edit(id);
+        const saveBtn = $(input.data("save-btn"));
+        editor.setTheme("ace/theme/eclipse");
+        editor.session.setMode("ace/mode/" + mode);
+        editor.on("change", () => {
+            if (!saveBtn[0]) return;
+            saveBtn.val("(Edited) Save")
+                .removeClass("btn-secondary")
+                .addClass("btn-warning");
+        });
+        // check json
+        form.on("submit", (e) => {
+            const value = editor.getValue();
+            if (mode === "json" && !isValidJson(value)) {
+                e.preventDefault();
+                window.alert("Please enter valid JSON.");
+                window.setTimeout(() => {
+                    form.find("input[type=submit]").prop("disabled", false);
+                }, 200);
+                return;
+            }
+            const hiddenInput = $(`<input type='hidden' name='${name}'/>`);
+            hiddenInput.appendTo(form);
+            hiddenInput.val(value);
+            saveBtn.val("Save")
+                .removeClass("btn-warning")
+                .addClass("btn-secondary");
+        });
+    });
 });
+
+function isValidJson(json) {
+    try {
+        JSON.parse(json);
+        return true;
+    } catch (e) {
+        return false;
+    }
+}
