405535d41821e44e98a7a2e2efde110fa159139e
WANG Yanfei <mintywangtvt@gmail.com>
2021-07-30 16:33:08 +0800

    updates for grid plot

--- app/javascript/viz/classifier/grid-plot.ts
+++ app/javascript/viz/classifier/grid-plot.ts
@@ -1,21 +1,28 @@
 import { Component, XYPlotOption } from "crux/dist/element";
 
-interface GridPlotOption extends XYPlotOption {
+export interface GridPlotOption extends XYPlotOption {
     xLabel: string;
     yLabel: string;
     plotSize: [number, number]; // [width, height]
     legend: any;
     discreteCategory: boolean;
+    yAxisRotated: boolean;
+    xAxisRotated: boolean;
+    labelOffsetVer: number;
+    labelOffsetHor: number;
+    labelFontSize: number;
+    tickFontSize: number;
 }
 
 export class GridPlot extends Component<GridPlotOption> {
 
+    protected xAxisRotation = 45;
     public render() {
         return this.t`
         XYPlot{
             @props prop
-            height = 30 + prop.plotSize[0]
-            width = 35 + prop.plotSize[1]
+            height = 30 + prop.plotSize[1]
+            width = 35 + prop.plotSize[0]
             padding-l = 35; padding-b = 30
             @yield background
             @yield content with prop.data
@@ -23,34 +30,64 @@ export class GridPlot extends Component<GridPlotOption> {
                 stroke = @color("line"); fill = "none"
             }
             @yield leftAxis default {
+            @if prop.yAxisRotated {
                 Axis("left") {
                     :label(tick) {
                         Text(tick.value) {
                             x = -5; anchor = @anchor("c", "b")
                             rotation = @rotate(-90)
-                            fontSize = 10;
+                            fontSize = prop.tickFontSize
                         }
                     }
                 }
+            } @else {
+                Axis("left") {label.fontSize = prop.tickFontSize}
+            }
             }
             @yield bottomAxis default {
+            @if prop.xAxisRotated {
                 Axis("bottom") {
                     y = 100%
+                    :label(tick) {
+                        Text(tick.value) {
+                            @let tickX = @scaled-x(tick.value)
+                            @let tickY = 5
+                            behavior:drag {
+                                direction = "polar"
+                                origin = [tickX, tickY]
+                                onDrag = @bind(adjustLabel)
+                            }
+                            y = 5; anchor = @anchor("r", "m")
+                            rotation = @rotate(-xAxisRotation)
+                            fontSize = prop.tickFontSize
+                        }
+                    }
                 }
+            } @else {
+                Axis("bottom") {
+                    y = 100%; label.fontSize = prop.tickFontSize
+                }
+            }
             }
             Text {
                 text = prop.flip ? prop.xLabel : prop.yLabel
-                x = -25; y = 50%; rotation = @rotate(-90)
-                fontSize = 12
+                x = -prop.labelOffsetVer; y = 50%; rotation = @rotate(-90)
+                fontSize = prop.labelFontSize
                 anchor = @anchor("m", "c")
             }
             Text {
                 text = prop.flip ? prop.yLabel : prop.xLabel
-                x = 50%; y = @geo(100,15)
-                fontSize = 12
+                x = 50%; y = @geo(100,prop.labelOffsetHor)
+                fontSize = prop.labelFontSize
                 anchor = @anchor("t", "c")
             }
         }`;
+    }
+
+    protected adjustLabel(ev, el, delta, cuur) {
+        this.xAxisRotation = cuur[0] * 180 / Math.PI;
+        // console.log(this.xAxisRotation);
+        this.redraw();
     }
 
     public defaultProp() {
@@ -58,6 +95,11 @@ export class GridPlot extends Component<GridPlotOption> {
             ...super.defaultProp(),
             xLabel: "",
             yLabel: "",
+            labelFontSize: 12,
+            tickFontSize: 10,
+            labelOffsetVer: 25,
+            labelOffsetHor: 15,
+            xAxisRotated: true,
         };
     }
 }
