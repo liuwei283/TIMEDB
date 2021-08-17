f5379ec15d1016840f376b66ba1c03485fc18441
WANG Yanfei <mintywangtvt@gmail.com>
2021-08-17 13:25:00 +0800

    updates on meta overview

--- app/javascript/viz/meta-overview/editor.ts
+++ app/javascript/viz/meta-overview/editor.ts
@@ -1,6 +1,5 @@
 import { event } from "crux/dist/utils";
 import { EditorDef, ToolbarDef } from "utils/editor";
-import { copyObject } from "utils/object";
 import { filterSamples } from "./data";
 
 export const editorRef: any = {};
@@ -8,26 +7,6 @@ export const editorRef: any = {};
 const conf: any = {
     sampleSortBy: ["age"],
 };
-
-function updateHistoData(v, idx) {
-    for (const h of v.data.data) {
-        let counter = 0;
-        const rev = [...v.data.histoKeys[idx]].reverse();
-        for (const hk of rev) {
-            const value = parseFloat(h[hk.rawKey]);
-            h._histo[idx][hk.key][0] = counter;
-            h._histo[idx][hk.key][1] = counter + value;
-            counter += value;
-        }
-    }
-    updateHistoLegendData(v);
-}
-
-function updateHistoLegendData(v) {
-    v.data.histoLegendData = v.data.histoKeys.map((h, i) => h.map(hk => ({ label: hk.key, fill: v.data.colorScales.histo[i].get(hk.key) })));
-}
-
-const samplesVueData: any = {};
 
 function updateSampleSorting(v, keys) {
     const getters = [];
@@ -68,29 +47,6 @@ function sort(data: any, getters: [any, boolean][]) {
     });
 }
 
-function selectGenes(genes: any[], str: string) {
-    if (str.length === 0 || str === "0") return genes;
-
-    const result = [];
-    for (const s of str.split(",")) {
-        const trimmed = s.trim();
-        if (trimmed.indexOf("-")) {
-            const [start_, end_] = trimmed.split("-").map(x => (x.length ? parseInt(x) : null));
-            if (!(start_ === null || start_ > 0)) return null;
-            if (!(end_ === null || end_ > 0)) return null;
-            const start = start_ === null ? 1 : start_;
-            const end = end_ === null ? undefined : end_;
-            result.push(...genes.slice(start - 1, end));
-        } else {
-            const g = genes[parseInt(trimmed) - 1];
-            if (!g) return null;
-            result.push(g);
-        }
-    }
-
-    return result;
-}
-
 function compare(a: any, b: any, getter: any, asc: boolean) {
     const a_ = getter(a),
         b_ = getter(b);
@@ -107,38 +63,10 @@ function update(v) {
 }
 
 export function editorConfig(v: any): EditorDef {
-    // conf.histoMaxValue = Array.from(v.data.histoMaxValue);
-    // conf.displaySampleLabel = Array.from(v.data.displaySampleLabel);
-    // conf.histoLabel = Array.from(v.data.histoLabel);
-    // conf.histoHidden = Array.from(v.data.histoHidden);
-    // conf.histoName = Array.from(v.data.histoName);
-    // conf.columnWidth = v.data.columnWidth;
-    // conf.genePanelRightInfo = v.data.geneInfo.display;
-    // conf.geneLeftLabel = v.data.geneLeftLabel;
-    // conf.geneLeftMaxValue = v.data.geneLeftMaxValue;
-    // conf.geneRightMaxValue = v.data.geneRightMaxValue;
-    // conf.qValueLine = v.data.qValueLine;
-
-    // Object.assign(samplesVueData, {
-    //     compact: true,
-    //     needAutoUpdate: true,
-    //     title: "Reorder samples manually",
-    //     array: Array.from(v.data.samples),
-    //     callback(d) {
-    //         v.data.samples = samplesVueData.array = d;
-    //         filterSamples(v);
-    //         update(v);
-    //     },
-    // });
-
-    // const panelDefs = v.data.panelKeyOrder.filter(key => key !== "pw").map(key => genPanelEditorDef(v, key, v.data.panelNames[key]));
 
     const d = v.data;
     const sampleReorderOpts = [
         ["id", "Sample ID"],
-        // ["bis", "Bisection"],
-        // ...d.histoKeys.map((h, i) => [`^hist_${i}`, `Histogram ${h[0].name}: Total`]),
-        // ...d.histoKeys.flat().map(k => [k.rawKey, k.key, `Histogram ${k.name}`]),
         ...d.metaFeatures.map(k => [`mt_${k}`, k, "Meta info"]),
     ].flatMap(([k, name, p]) => [
             { value: `a${k}`, text: `${p ? `${p}: ` : ""}${name} ↑` },
@@ -158,34 +86,47 @@ export function editorConfig(v: any): EditorDef {
                         view: {
                             type: "list",
                             items: [
-                                // {
-                                //     title: "Grid width",
-                                //     type: "input",
-                                //     format: "int",
-                                //     bind: {
-                                //         object: conf,
-                                //         path: "columnWidth",
-                                //         callback() {
-                                //             v.data.columnWidth = parseFloat(conf.columnWidth);
-                                //             update(v);
-                                //         },
-                                //     },
-                                // },
                                 {
-                                    type: "vue",
-                                    component: "color-picker",
-                                    ref: "naColorPicker",
-                                    data: {
-                                        title: "Color for N/A",
-                                        scheme: {},
-                                        naColor: v.data.NAColor,
-                                        id: "group",
-                                        callback(_, naColor) {
-                                            v.data.NAColor = naColor;
+                                    title: "Grid width",
+                                    type: "input",
+                                    format: "int",
+                                    value: {
+                                        current: v.data.gridSize[0],
+                                        callback(x) {
+                                            v.data.gridSize[0] = parseFloat(x);
+                                            v.data._sizeUpdated = true;
                                             update(v);
                                         },
                                     },
                                 },
+                                {
+                                    title: "Grid height",
+                                    type: "input",
+                                    format: "int",
+                                    value: {
+                                        current: v.data.gridSize[1],
+                                        callback(x) {
+                                            v.data.gridSize[1] = parseFloat(x);
+                                            v.data._sizeUpdated = true;
+                                            update(v);
+                                        },
+                                    },
+                                },
+                                // {
+                                //     type: "vue",
+                                //     component: "color-picker",
+                                //     ref: "naColorPicker",
+                                //     data: {
+                                //         title: "Color for N/A",
+                                //         scheme: {},
+                                //         naColor: v.data.NAColor,
+                                //         id: "group",
+                                //         callback(_, naColor) {
+                                //             v.data.NAColor = naColor;
+                                //             update(v);
+                                //         },
+                                //     },
+                                // },
                             ],
                         },
                     },
@@ -289,122 +230,6 @@ export function editorConfig(v: any): EditorDef {
                         },
                     },
                 ],
-            },
-        ],
-    };
-}
-
-function histoPanelDef(v: any, h: any, i: number) {
-    return {
-        id: `histo-${i}`,
-        title: `Histogram ${h[0].name}`,
-        layout: "tabs",
-        tabs: [
-            {
-                id: `h-settings-${i}`,
-                name: "Settings",
-                view: {
-                    type: "list",
-                    items: [
-                        {
-                            title: "Name",
-                            type: "input",
-                            bind: {
-                                object: conf,
-                                path: `histoName.#${i}`,
-                                callback() {
-                                    v.data.histoName[i] = conf.histoName[i];
-                                    update(v);
-                                },
-                            },
-                        },
-                        {
-                            title: "Hidden",
-                            type: "checkbox",
-                            bind: {
-                                object: conf,
-                                path: `histoHidden.#${i}`,
-                                callback() {
-                                    v.data.histoHidden[i] = conf.histoHidden[i];
-                                    update(v);
-                                },
-                            },
-                        },
-                        {
-                            title: "Display sample labels",
-                            type: "checkbox",
-                            bind: {
-                                object: conf,
-                                path: `displaySampleLabel.#${i}`,
-                                callback() {
-                                    v.data.displaySampleLabel[i] = conf.displaySampleLabel[i];
-                                    update(v);
-                                },
-                            },
-                        },
-                        {
-                            title: "Max value for Y axis",
-                            type: "input",
-                            format: "float",
-                            bind: {
-                                object: conf,
-                                path: `histoMaxValue.#${i}`,
-                                callback() {
-                                    v.data.histoMaxValue[i] = parseInt(conf.histoMaxValue[i]);
-                                    update(v);
-                                },
-                            },
-                        },
-                        {
-                            title: "Y axis label",
-                            type: "input",
-                            bind: {
-                                object: conf,
-                                path: `histoLabel.#${i}`,
-                                callback() {
-                                    v.data.histoLabel[i] = conf.histoLabel[i];
-                                    update(v);
-                                },
-                            },
-                        },
-                    ],
-                },
-            },
-            {
-                name: "Data",
-                id: `h-data-${i}`,
-                view: {
-                    type: "list",
-                    items: [
-                        {
-                            type: "vue",
-                            component: "reorder",
-                            data: {
-                                title: "Reorder groups in histogram",
-                                array: v.data.histoKeys[i].map(h => h.key),
-                                callback(array) {
-                                    v.data.histoKeys[i] = array.map(x => v.data.histoKeys[i].find(h => h.key === x));
-                                    updateHistoData(v, i);
-                                    update(v);
-                                },
-                            },
-                        },
-                        {
-                            type: "vue",
-                            component: "color-picker",
-                            data: {
-                                title: "Customize colors",
-                                scheme: copyObject(v.data.colorScales.histo[i].colors),
-                                id: "histo",
-                                callback(colors) {
-                                    v.data.colorScales.histo[i].colors = colors;
-                                    updateHistoLegendData(v);
-                                    update(v);
-                                },
-                            },
-                        },
-                    ],
-                },
             },
         ],
     };
