b978fd368809b91572a9269e1dc372302f9d3fdc
minty <mintywangtvt@gmail.com>
2021-11-29 17:28:10 +0800

    add task monitor for admin

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,4 +3,33 @@ class Task < ApplicationRecord
     belongs_to :analysis, optional: true
     belongs_to :analysis_pipeline, optional: true
     has_many :task_outputs, dependent: :destroy
+
+    def getBrief
+
+        return "Task-#{id} (#{self.getAppName}, #{self.status}, user-#{self.user.id})"
+    end
+
+    def getAppName
+        @app_name = ""
+        if self.analysis
+            @app_name = self.analysis.name
+        else 
+            @app_name = self.analysis_pipeline.name
+        end
+        return @app_name
+    end
+
+    def getAppId
+        @app_id = ""
+        if self.analysis
+            @app_id = self.analysis.mid.string
+        else 
+            analysis_ids = self.analysis_pipeline.analyses.map do |a|
+                "#{a.name}: #{a.mid}"
+            end
+            @app_id = "#{self.analysis_pipeline.pid} (#{analysis_ids.join(", ")})"
+        end
+        return @app_id
+    end
+
 end
