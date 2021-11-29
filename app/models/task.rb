77409361836e9c795922181745748dbdc771c856
minty <mintywangtvt@gmail.com>
2021-11-29 17:58:54 +0800

    debug

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,30 +5,38 @@ class Task < ApplicationRecord
     has_many :task_outputs, dependent: :destroy
 
     def getBrief
-
         return "Task-#{id} (#{self.getAppName}, #{self.status}, user-#{self.user.id})"
     end
 
     def getAppName
         @app_name = ""
-        if self.analysis
-            @app_name = self.analysis.name
-        else 
-            @app_name = self.analysis_pipeline.name
+        begin
+            if !self.analysis.blank?
+                @app_name = self.analysis.name
+            else 
+                @app_name = self.analysis_pipeline.name
+            end
+        rescue Exception => e
+            @app_name = e.message
         end
         return @app_name
     end
 
     def getAppId
         @app_id = ""
-        if self.analysis
-            @app_id = self.analysis.mid.string
-        else 
-            analysis_ids = self.analysis_pipeline.analyses.map do |a|
-                "#{a.name}: #{a.mid}"
+        begin
+            if !self.analysis.blank?
+                @app_id = self.analysis.mid.string
+            else 
+                analysis_ids = self.analysis_pipeline.analyses.map do |a|
+                    "#{a.name}: #{a.mid}"
+                end
+                @app_id = "#{self.analysis_pipeline.pid} (#{analysis_ids.join(", ")})"
             end
-            @app_id = "#{self.analysis_pipeline.pid} (#{analysis_ids.join(", ")})"
+        rescue Exception => e
+            @app_id = e.message
         end
+        
         return @app_id
     end
 
